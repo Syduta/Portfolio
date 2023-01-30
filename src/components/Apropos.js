9f8b93873e67c71d3365834472f8c62f1b494985
Thibault Touzin <thibault.touzin@gmail.com>
2023-01-30 20:58:02 +0100

    more apropos things

--- src/components/Apropos.js
+++ src/components/Apropos.js
@@ -22,13 +22,28 @@ const Apropos = () => {
     return(
         <>
             <section id={"apropos"}>
-                <div className={"description"}>
-                    <h3>À propos</h3>
-                    {/*<p>Développeur passionné depuis mes premières lignes de code suite à une reconversion en 2022. </p>
-                    <p>Déterminé à évoluer dans le développement web je suis curieux, persévérant, minutieux et
-                        impatient de travailler avec vous ! </p>
-                    <p>J'aime autant coder le back que le front. Chaque projet étant différent d'un autre je trouve
-                        toujours intéressant d'en découvrir et de résoudre les problématiques. </p>*/}
+                <h3>À propos</h3>
+                <div className={"truc-container"}>
+                    <div className={"description"}>
+                        <p>Développeur passionné depuis mes premières lignes de code suite à une reconversion en 2022. </p>
+                        <p>J'aime autant coder le back que le front. Chaque projet étant différent d'un autre je trouve
+                            toujours intéressant de découvrir et d'en résoudre les problématiques. </p>
+                        <p>Déterminé à évoluer dans le développement web je suis curieux, persévérant, minutieux et
+                            impatient de travailler avec vous ! </p>
+                    </div>
+                    <div className={"competences"}>
+                        <p>Après avoir fait le tour de ma roue des technos je vous laisse découvrir les compétences
+                            que je souhaite mettre à vôtre disposition.</p>
+                        <ul>Pour la partie front-end :
+                            <li>Le maquetage.</li>
+                            <li>La création d'une interface statique et adaptable.</li>
+                            <li>Ou d'une interface dynamique.</li>
+                        </ul>
+                        <ul>Pour la partie back-end :
+                            <li>La création d'une base de données.</li>
+                            <li>le développement de composants d'accès à ces données.</li>
+                        </ul>
+                    </div>
                 </div>
                 <CircularMenu />
             </section>

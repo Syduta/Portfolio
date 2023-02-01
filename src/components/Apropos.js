7dd98ff535dde824ccd79ccde4b2fd70fa3cce77
Thibault Touzin <thibault.touzin@gmail.com>
2023-02-01 13:24:52 +0100

    apropos again

--- src/components/Apropos.js
+++ src/components/Apropos.js
@@ -29,14 +29,14 @@ const Apropos = () => {
                         <p>J'aime autant coder le back que le front. Chaque projet étant différent d'un autre je trouve
                             toujours intéressant de découvrir et d'en résoudre les problématiques. </p>
                         <p>Déterminé à évoluer dans le développement web je suis curieux, persévérant, minutieux et
-                            impatient de travailler avec vous ! </p>
+                            le plus important, impatient de travailler avec vous ! </p>
                     </div>
                     <div className={"competences"}>
                         <p>Après avoir fait le tour de ma roue des <em>technos</em> je vous laisse découvrir mes <em className={"emphase"}>compétences</em>.</p>
                         <ul><span className={"fullstack"}>Front-end</span>
                             <li>Le maquetage.</li>
                             <li>La création d'une interface statique et adaptable.</li>
-                            <li>Ou d'une interface dynamique.</li>
+                            <li>D'une interface dynamique.</li>
                         </ul>
                         <ul><span className={"fullstack"}>Back-end</span>
                             <li>La création d'une base de données.</li>

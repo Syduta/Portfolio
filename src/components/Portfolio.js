371507a14cff2873b540d81b2137c529651fcbf0
Thibault Touzin <thibault.touzin@gmail.com>
2023-02-20 18:37:02 +0100

    figcaption portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,15 +1,21 @@
+import {useState} from "react";
+
 const Portfolio = () => {
+    const [showcaption, setshowCaption] = useState(false);
     return(
         <>
             <section id={"portfolio"}>
                 <h3>Portfolio</h3>
                 <div className={"project-container"}>
-                    <div className={"projet newworld"}></div>
-                    <div className={"projet underground"}></div>
-                    <div className={"projet peps"}></div>
-                    <div className={"projet reno"}></div>
-                    <div className={"projet wow"}></div>
-                    <div className={"projet"}></div>
+                    <div className={"projet newworld"}><div
+                        onMouseEnter={() => {setshowCaption(true);}}
+                        onMouseLeave={() => {setshowCaption(false);}}
+                    ><figcaption>{showcaption && <h1>Hello, World!</h1>}</figcaption></div></div>
+                    <div className={"projet underground"}><div><figcaption>coucou</figcaption></div></div>
+                    <div className={"projet peps"}><div><figcaption>coucou</figcaption></div></div>
+                    <div className={"projet reno"}><div><figcaption>coucou</figcaption></div></div>
+                    <div className={"projet wow"}><div><figcaption>coucou</figcaption></div></div>
+                    <div className={"projet"}><div><figcaption>coucou</figcaption></div></div>
                 </div>
             </section>
         </>

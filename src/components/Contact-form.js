0431d757de22e7dbc43732f1c961a969df4f0414
Thibault Touzin <thibault.touzin@gmail.com>
2023-02-24 17:27:30 +0100

    kind of a form

--- src/components/Contact-form.js
+++ src/components/Contact-form.js
@@ -1,16 +1,25 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faEnvelope, faMessage } from "@fortawesome/free-solid-svg-icons";
 
-
-
+const user = <FontAwesomeIcon icon={faUser} fontSize={"1em"} />;
+const email = <FontAwesomeIcon icon={faEnvelope} fontSize={"1em"} />;
+const message = <FontAwesomeIcon icon={faMessage} fontSize={"1em"} />;
 const ContactForm = () => {
     return(
         <>
             <form>
-                <label>Nom</label>
-                <input type={"text"} placeholder={"Votre prénom et nom"} name={"user_name"} required />
-                <label>Email</label>
-                <input type={"email"} placeholder={"Votre adresse email"} name={"user_email"} required />
-                <label>Message</label>
-                <textarea name={"message"} placeholder={"Le message à envoyer"} />
+                <div className={"input-container"}>
+                    <label>{user}</label>
+                    <input type={"text"} placeholder={"Votre prénom et nom"} name={"user_name"} required />
+                </div>
+                <div className={"input-container"}>
+                    <label>{email}</label>
+                    <input type={"email"} placeholder={"Votre adresse email"} name={"user_email"} required />
+                </div>
+                <div className={"input-container"}>
+                    <label>{message}</label>
+                    <textarea name={"message"} placeholder={"Le message à envoyer"} />
+                </div>
                 <input type={"submit"} value={"Envoyer"} required />
             </form>
         </>
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,9 +2,9 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin, faGithub, faDiscord} from '@fortawesome/free-brands-svg-icons';
 import ContactForm from "./Contact-form";
 
-const fontAwesomeLinkedin = <FontAwesomeIcon icon={faLinkedin} color={"#5AB0F7"}/>;
-const fontAwesomeGithub = <FontAwesomeIcon icon={faGithub} color={"#161414"}/>;
-const fontAwesomeDiscord = <FontAwesomeIcon icon={faDiscord} color={"#5460E6"}/>
+const fontAwesomeLinkedin = <FontAwesomeIcon icon={faLinkedin} color={"#5AB0F7"} fontSize={"1.5em"}/>;
+const fontAwesomeGithub = <FontAwesomeIcon icon={faGithub} color={"#161414"} fontSize={"1.5em"}/>;
+const fontAwesomeDiscord = <FontAwesomeIcon icon={faDiscord} color={"#5460E6"} fontSize={"1.5em"}/>
 const Contact = () => {
     return(
         <>
@@ -13,10 +13,13 @@ const Contact = () => {
                 <p>Je suis joignable par mail,</p>
                 <ContactForm />
                 <p>Ou sur </p>
-                <a>{fontAwesomeLinkedin}</a>
-                <a>{fontAwesomeGithub}</a>
-                <a>{fontAwesomeDiscord}</a>
-            </section>
+                <div>
+                    <a href={"https://www.linkedin.com/in/thibaulttouzin/"} target={"_blank"} rel="noreferrer" >{fontAwesomeLinkedin}</a>
+                    <a href={"https://github.com/Syduta"} target={"_blank"} rel="noreferrer" >{fontAwesomeGithub}</a>
+                    <a href={"https://discordapp.com/users/634639038766383105/"} target={"_blank"} rel="noreferrer" >{fontAwesomeDiscord}</a>
+
+                </div>
+        </section>
         </>
     )
 }

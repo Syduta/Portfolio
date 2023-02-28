a9e54ad87aac71adbca4677538e82c41c8257532
Thibault Touzin <thibault.touzin@gmail.com>
2023-02-28 12:37:31 +0100

    better form

--- src/components/Contact-form.js
+++ src/components/Contact-form.js
@@ -10,15 +10,15 @@ const ContactForm = () => {
             <form>
                 <div className={"input-container"}>
                     <label>{user}</label>
-                    <input type={"text"} placeholder={"Votre prénom et nom"} name={"user_name"} required />
+                    <input type={"text"} placeholder={`Votre prénom et nom`} aria-label={"Entrez votre prénom et nom"} name={"user_name"} required />
                 </div>
                 <div className={"input-container"}>
                     <label>{email}</label>
-                    <input type={"email"} placeholder={"Votre adresse email"} name={"user_email"} required />
+                    <input type={"email"} placeholder={"Votre adresse email"} aria-label={"Entrez votre adresse email"} name={"user_email"} required />
                 </div>
                 <div className={"input-container"}>
                     <label>{message}</label>
-                    <textarea name={"message"} placeholder={"Le message à envoyer"} />
+                    <textarea name={"message"} placeholder={"Le message à envoyer"} aria-label={"Tapez votre message"} />
                 </div>
                 <input type={"submit"} value={"Envoyer"} required />
             </form>
